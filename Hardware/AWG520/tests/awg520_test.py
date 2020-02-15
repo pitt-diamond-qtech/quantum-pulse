0ebc5b3203c3575cb92b9255e8b7ad5217431358
Gurudev Dutt <gurudev.dutt@gmail.com>
2020-02-15 17:36:13 -0500

    major improvement to AWG testing, dummy server can now be run inside pytest

--- Hardware/AWG520/tests/awg520_test.py
+++ Hardware/AWG520/tests/awg520_test.py
@@ -1,75 +1,63 @@
 # Created on 2/1/20 by gurudev
-
-from pathlib import Path
-import os
-from Pulseshaping.Hardware.AWG520 import AWG520
+from Hardware.AWG520 import AWG520 # looks like python has no problem finding this when the top-level directory is
+# the same as the project name
+import subprocess
+import socket
+import time
 import pytest
-from multiprocessing import Pipe,Process
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost), change to '179.17.39.2' for device
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023), change to 4000 for device
 FTP_PORT = 63217    # Port for ftp testing, change to 21 for device
 
-print('Module name is: ',__name__)
-import socket
 
-# def run_echo_server():
-#     with open('echo-server.py') as f:
-#         code = compile(f.read(),'echo-server.py','exec')
-#         exec(code)
-
-def echo_server(c_conn,host=HOST,port=PORT):
-    import socket
-
-    # this server simulates the AWG command protocol, simply echoing the command back except for IDN?
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        try:
-            while True:
-                c_conn.send('waiting for connection')
-                conn, addr = s.accept()
-                with conn:
-                    c_conn.send('Connected by', addr)
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    elif (data == b'*IDN?\n'):
-                        print('SONY/TEK,AWG520,0,SCPI:95.0 OS:3.0 USR:4.0\n')
-                        conn.sendall(b'SONY/TEK,AWG520,0,SCPI:95.0 OS:3.0 USR:4.0\n')
-                        c_conn.send(b'SONY/TEK,AWG520,0,SCPI:95.0 OS:3.0 USR:4.0\n')
-                    else:
-                        conn.sendall(data)
-                        c_conn.send(data)
-        finally:
-            conn.close()
+@pytest.fixture(scope="session")
+def awgserver():
+    print("loading server")
+    p = subprocess.Popen(["python3", "awg_dummy_server.py"])
+    time.sleep(1)
+    yield p
+    p.terminate()
 
 
-def start_echo_server():
-    parent_conn, child_conn = Pipe()
-    p = Process(target=echo_server, args=(child_conn,HOST,PORT),daemon=True)
-    p.start()
-    print(parent_conn.recv())
-    p.join(1)
-    return p
+@pytest.fixture
+def clientsocket(request):
+    print("entering client part")
+    server = getattr(request.module, "HOST")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as mysock:
+        mysock.connect((HOST, PORT))
+        yield mysock
+        print("finalizing {} ({})".format(mysock, server))
+        mysock.close()
+
+# first we test the very basic socket communication which requires strings to be encoded
+@pytest.mark.run_this
+def test_echo(awgserver, clientsocket):
+    clientsocket.send(b"*IDN?\n")
+    assert clientsocket.recv(1024) == b"SONY/TEK,AWG520,0,SCPI:95.0 OS:3.0 USR:4.0\n"
+    #assert clientsocket.recv(10) == b"TEK" # deliberately make test fail
 
 
+@pytest.mark.run_this
+def test_echo2(awgserver, clientsocket):
+    clientsocket.send(b"def")
+    assert clientsocket.recv(3) == b"def"
+
+# now we test the AWG520 class
+@pytest.mark.run_this
 def test_awg520():
-    '''Prior to running pytest on this folder you should start the echo-server and teh ftp-server.
-    eventually i need to start those servers from within the testing script but for now this works.'''
-
-
-    try:
-        # proc = start_echo_server()
+    with pytest.raises(AssertionError):
         c = AWG520(HOST,PORT)
-        c.setup()
-        print('current directory is',os.getcwd())
-        #c.sendfile('0_2.wfm','../sequencefiles/0_2.wfm') # testing the FTP file transfer
-        c.green_off()
-        # proc.close()
-    except:
-        raise
+        reply = c.set_clock_external()
+        assert reply == 'TEK' # deliberate fail to show that pytest will not flag this since it is expected
 
+@pytest.mark.run_this
+def test_awg520_2():
+    c = AWG520(HOST,PORT)
+    reply = c.sendcommand('TRG?')
+    assert reply == 'TEK' # deliberate fail to show that pytest will flag this
+
+# when pytest is run with the '-m run_this' flag this test will not be executed
 def test_awgfile():
     d = AWGFile(ftype="SEQ")
     from matplotlib import pyplot as plt
