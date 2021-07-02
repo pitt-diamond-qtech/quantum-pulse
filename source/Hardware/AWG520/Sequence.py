279e4a4bcd2ec14ae19c13c1bb1d076bafcb6a12
Gurudev Dutt <55997000+gurudevdutt@users.noreply.github.com>
2021-07-02 17:48:50 -0400

    adding in code for randomization

--- source/Hardware/AWG520/Sequence.py
+++ source/Hardware/AWG520/Sequence.py
@@ -537,6 +537,113 @@ class RandomPauliGate(WaveEvent):
         if pulsetype is None:
             pass
 
+    def generate_computation_gates(self):
+        """
+        we have 3 types of pulses to generate:
+        Pauli Gates (pi_x, pi_y and pi_z), Computational Gates (pi/2_x, pi/2_y, pi/2_z) and the R gate (This is a custom gate to make sure the
+        final measurement is an eigenstate of sigma_z)
+
+        Parameters:
+        Nl = Number of different truncation lengths
+        Ng = Different computational gate sequences
+        Np = Number of Pauli Randomization
+        Ne = Number of Total experiments
+
+        """
+        l_max = 100  # Number of Computational gates that will be generated in each of the Ng sequences and then truncated Nl times.
+        P = ['(identity)', '(+pi_x)', '(+pi_y)', '(-pi_x)', '(-pi_y)']  # The Set of Pauli gates
+        G = ['(+pi/2_x)', '(+pi/2_y)', '(-pi/2_x)', '(-pi/2_y)']  # The set of Comp. gates
+        L = [2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 24, 32, 40, 48, 64, 80, 96]  # The list of different truncation lengths
+        l = 4
+        """
+        Generate the Ng computational gate sequences
+        """
+        Comp_seq_list = [[], [], [], []]
+        for seq in Comp_seq_list:
+            for i in range(l_max):
+                seq.append(random.choice(G))
+        """
+        Choose one of the Ng sequences randomly truncate it to length l (should be done to all Ng sequences, not just one sequence)
+        """
+        G_list = Comp_seq_list[random.randint(0, 3)][:l]
+
+        """Generate a list of l+2 Pauli gates randomly"""
+        P_list = []
+        for i in range(l + 2):
+            P_list.append(random.choice(P))
+        # print(P_list)
+        # print(G_list)
+
+        """"Pauli operators and their eigenstates"""
+        I = np.array([[1, 0], [0, 1]])  # Identity
+        X = np.array([[0, 1], [1, 0]])  # Sx
+        Y = np.array([[0, -1j], [1j, 0]])  # Sy
+        Z = np.array([[1, 0], [0, -1]])  # Sz
+
+        z0 = np.array([[1], [0]])  # |+z>
+        z1 = np.array([[0], [1]])  # |-z>
+        x0 = np.round(np.array([[1], [1]]) / np.sqrt(2), 3)  # |+x>
+        x1 = np.round(np.array([[1], [-1]]) / np.sqrt(2), 3)  # |-x>
+        y0 = np.round(np.array([[1], [1j]]) / np.sqrt(2), 3)  # |+y>
+        y1 = np.round(np.array([[1], [-1j]]) / np.sqrt(2), 3)  # |-y>
+
+        def rotation(spin_axis, angle):  # takes a rotation axis and angle and returns a rotation matrix
+            # return round(np.cos(angle/2),3)*I -1j* round(np.sin(angle/2),3)*spin_axis
+            return np.cos(angle / 2) * I - 1j * np.sin(angle / 2) * spin_axis
+
+        gate = {'(identity)': I,
+                '(+pi_x)': rotation(X, np.pi),
+                '(-pi_x)': rotation(-1 * X, np.pi),
+                '(+pi_y)': rotation(Y, np.pi),
+                '(-pi_y)': rotation(-1 * Y, np.pi),
+                '(+pi/2_x)': rotation(X, np.pi / 2),
+                '(-pi/2_x)': rotation(-1 * X, np.pi / 2),
+                '(+pi/2_y)': rotation(Y, np.pi / 2),
+                '(-pi/2_y)': rotation(-1 * Y, np.pi / 2),
+                '(+pi_z)': rotation(Z, np.pi),
+                '(-pi_z)': rotation(-1 * Z, np.pi),
+                '(+pi/2_z)': rotation(Z, np.pi / 2),
+                '(-pi/2_z)': rotation(-1 * Z, np.pi / 2)
+                }
+        r_gate = {'(+pi/2_x)': rotation(X, np.pi / 2),
+                  '(-pi/2_x)': rotation(-1 * X, np.pi / 2),
+                  '(+pi/2_y)': rotation(Y, np.pi / 2),
+                  '(-pi/2_y)': rotation(-1 * Y, np.pi / 2),
+                  '(+pi/2_z)': rotation(Z, np.pi / 2),
+                  '(-pi/2_z)': rotation(-1 * Z, np.pi / 2)
+                  }
+
+        def find_R(gate_list):
+            M = I
+            Rlist = []
+            for i in gate_list:
+                M = np.matmul(gate[i], M)
+            for i in r_gate:
+                M2 = np.matmul(r_gate[i], M)
+                if np.allclose(abs(np.inner(np.ndarray.flatten(z0), np.ndarray.flatten(np.matmul(M2, z0)))),
+                        1) or np.allclose(
+                        abs(np.inner(np.ndarray.flatten(z1), np.ndarray.flatten(np.matmul(M2, z0)))), 1):
+                    Rlist.append(i)
+            R = random.choice(Rlist)
+            M3 = np.matmul(r_gate[R], M)
+            if np.isclose(abs(np.inner(np.ndarray.flatten(z0), np.ndarray.flatten(np.matmul(M3, z0)))), 1):
+                final_state = 'z0'
+            elif np.isclose(abs(np.inner(np.ndarray.flatten(z1), np.ndarray.flatten(np.matmul(M3, z0)))), 1):
+                final_state = 'z1'
+            else:
+                print("error!!!")
+            return R, final_state
+
+        """"Generate the full sequence"""
+        R, final_state = find_R(G_list)
+        sequence = []
+        for i in range(l):
+            sequence.append(P_list[i])
+            sequence.append(G_list[i])
+        sequence.append(P_list[-2])
+        sequence.append(R)
+        sequence.append(P_list[-1])
+
 
 
 class MarkerEvent(SequenceEvent):
